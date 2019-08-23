622beace257994506b7f55f5c0d248255a6753a1
Panela Leung <leungpanela@gmail.com>
2019-08-23 23:44:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,5 @@
 require "pry"
 # Write your code here.
 def badge_maker(name)
-    puts "Hello, my name is #{name}."
-end
-
-def batch_badge_creator (array)
-  puts "Hello, #{name}! You'll be assigned to room #{room}!"
+    return "Hello, my name is #{name}."
 end
