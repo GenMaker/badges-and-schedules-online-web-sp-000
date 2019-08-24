55acc067af4d4911f90227f86a5aa1cce494fdaa
Panela Leung <leungpanela@gmail.com>
2019-08-24 02:32:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,20 +14,20 @@ end
 def assign_rooms(speakers)
   speakers.map_with_index do |speakers|
   room_assignments << "Hello, #{speakers}! You'll be assigned to room #{index + 1}!"
-end
+  end
 end
 
 
 
-def assign_rooms(attendees)
-  room = 1
-  room_assignments=[]
-  attendees.each do |attendees|
-    room_assignments << "Hello, #{attendees}! You'll be assigned to room #{room}!"
-    room += 1
-end
-return room_assignments
-end
+#def assign_rooms(attendees)
+  #room = 1
+  #room_assignments=[]
+  #attendees.each do |attendees|
+    #room_assignments << "Hello, #{attendees}! You'll be assigned to room #{room}!"
+    #room += 1
+#end
+#return room_assignments
+#end
 
 def printer(array)
   puts batch_badge_creator(array)
