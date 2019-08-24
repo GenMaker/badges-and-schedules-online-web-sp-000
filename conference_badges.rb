26878ecec5124c96bca18fc63cb4d788ca70af6b
Panela Leung <leungpanela@gmail.com>
2019-08-24 00:34:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
     return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(name)
   badge = []
    array.each do |name|
     badge << badge_maker(name)
