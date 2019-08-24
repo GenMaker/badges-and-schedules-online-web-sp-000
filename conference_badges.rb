064127111d94484184ba2e42f7e19a0708aa2563
Panela Leung <leungpanela@gmail.com>
2019-08-24 00:29:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,12 +12,14 @@ def batch_badge_creator(array)
   return badge
 end
 
-def assign_rooms(name,array)
+def assign_rooms(name)
   rooms = 1
   room_assignments=[]
   array.each do |room|
-    puts "Hello #{name}! "
-  rooms += 1
+    room_assignments << "Hello,#{name}! You'll be assigned to room #{room}!"
+  while rooms > 7
+    rooms += 1
+  end
 return room_assginments
   end
 end
