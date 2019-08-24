93032094ddf686aded15c14f9ae097e074e80173
Panela Leung <leungpanela@gmail.com>
2019-08-24 02:35:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(speakers)
   speakers.map.with_index do |name, index|
-    "Hello, #{speakers}! You'll be assigned to room #{index + 1}!"
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
 
